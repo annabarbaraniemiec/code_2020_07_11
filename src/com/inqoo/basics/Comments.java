b22c9bd00bc23ff0543038f610966c31994a92f0
Dell <a.dudek1@gmail.com>
2020-07-11 12:36:36 +0200

    dodana nazwa, usunięty komentarz

--- src/com/inqoo/basics/Comments.java
+++ src/com/inqoo/basics/Comments.java
@@ -3,7 +3,7 @@ package com.inqoo.basics;
 public class Comments {
 
     public static void main(String[] args) {
-        System.out.println("Witaj na ") //Powitanie
+        System.out.println("Witaj na "); //Powitanie
         System.out.println("kursie programowania INQOO!!!");
 
         /* Na koniec emotka,
